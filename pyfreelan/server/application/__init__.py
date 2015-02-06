9db885ee08ac5a3350b0ab70761bc1b378bb7e4c
Julien Kauffmann <julien.kauffmann@freelan.org>
2015-02-05 19:10:46 -0500

    Added logs when calling views

--- pyfreelan/server/application/__init__.py
+++ pyfreelan/server/application/__init__.py
@@ -10,16 +10,51 @@ from flask import (
     g,
     make_response,
 )
-from flask.ext.login import login_required
+from flask.ext.login import (
+    login_required,
+    current_user,
+)
+from functools import wraps
 
 from .security import LOGIN_MANAGER
 from .exceptions import (
     HTTPException,
     UnexpectedFormat,
 )
+from ...log import LOGGER
 
 APP = Flask('pyfreelan')
 LOGIN_MANAGER.init_app(APP)
+
+
+def log_activity(func):
+    """
+    Log any activity of the decorated request.
+
+    :param func: The function to decorate.
+    :returns: The decorated function.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        LOGGER.debug(
+            "Web request from '%s' to '%s' (%s).",
+            current_user.get_id(),
+            request.url,
+            request.method,
+        )
+
+        response = func(*args, **kwargs)
+
+        LOGGER.debug(
+            "Web response to '%s' from '%s': %s\n%s",
+            current_user.get_id(),
+            request.url,
+            response.status,
+            response.data,
+        )
+        return response
+
+    return wrapper
 
 
 @APP.errorhandler(HTTPException)
@@ -28,6 +63,7 @@ def handle_unexpected_format(error):
 
 
 @APP.route('/')
+@log_activity
 @login_required
 def index():
     return jsonify(
@@ -42,6 +78,7 @@ def index():
 
 
 @APP.route('/request_certificate/', methods={'POST'})
+@log_activity
 @login_required
 def request_certificate():
     try:
